aa72a37dee05133dcc7940a44bb524b9397207ea
Alex Sutton <mandarinfish27@aol.com>
2023-10-09 16:51:51 -0600

    nears completion on profile and assessment overall

--- about.js
+++ about.js
@@ -1,14 +1,17 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Submission sent successfully.');
 }
 
+let cat = document.querySelector("#cat");
+cat.addEventListener("mouseover" , function(){
+	alert("You're looking nice today...");
+})
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
